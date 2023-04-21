8ca1ac64b383467f4acb3505d42a9ac2acad6c52
nadar99 <rnada260@gmail.com>
2023-04-21 06:50:57 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
