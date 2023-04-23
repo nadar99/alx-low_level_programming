80f774f460f83e344d9b191b2e87185a0b0f3077
nadar99 <rnada260@gmail.com>
2023-04-23 16:19:24 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, x, y;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (x = i; x <= 57; x++)
+			{
+				for (y = j + 1; y <= 57; y++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(x);
+					putchar(y);
+					if ((i != 57) || (j != 56) || (x != 57) || (y != 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
