b8537aa2e44937613b0b5708dd280f7a8525dacc
nadar99 <rnada260@gmail.com>
2023-04-25 13:21:16 -0700

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - print the add of two numbers
+ * @x: The number to check
+ * @y: The number to check
+ *
+ * Return: int.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
