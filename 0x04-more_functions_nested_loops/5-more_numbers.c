f30a8c20b6140fe4d99056ab624c2724beba114e
nadar99 <rnada260@gmail.com>
2023-04-27 09:35:13 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+		{
+			_putchar((i / 10) + '0');
+		}
+		_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+}
