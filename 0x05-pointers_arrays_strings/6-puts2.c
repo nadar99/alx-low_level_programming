8d4ebd188a6525ce45e6b0f620bbd55d0d0d5170
nadar99 <rnada260@gmail.com>
2023-05-02 07:03:28 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - print every other char in a string
+ *@str : pointer to a charachter
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int size = 0, i = 0;
+	char *str2 = str;
+
+	while (*str2 != '\0')
+	{
+		size++;
+		str2++;
+	}
+	while (i < (size / 2))
+	{
+		_putchar(*str);
+		str++;
+		str++;
+		i++;
+	}
+	_putchar('\n');
+}
