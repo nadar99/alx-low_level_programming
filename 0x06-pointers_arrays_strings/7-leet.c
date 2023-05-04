13d2a584c8daea325f3dcd27ade81f54b5028ae0
nadar99 <rnada260@gmail.com>
2023-05-04 06:25:06 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+ * leet - change string to uppercase
+ *@s : the array
+ * Return: string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char arr[] = {'0', 'l', '@', 'e', 'a', '@', '@', 't'};
+
+	while (s[i])
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (s[i] == arr[j] || s[i] == (arr[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
