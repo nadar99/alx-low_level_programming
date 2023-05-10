26b481319f3d950562269d04950f76da6ae0a65e
nadar99 <rnada260@gmail.com>
2023-05-10 05:52:40 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - a function that returns the factorial of a number
+ *@n: number
+ * Return: int.
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
