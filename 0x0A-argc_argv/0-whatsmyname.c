d0b1910498c51ceac3904f46ccd681cbd24c9e55
nadar99 <rnada260@gmail.com>
2023-05-12 03:38:37 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'the program's description
+ *@argc :argumanets count
+ *@argv: array of argumanets strings
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
