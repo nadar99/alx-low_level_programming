9e2e2df8fdd6fe68581f51bcf4b27d6d278cbdfc
nadar99 <rnada260@gmail.com>
2023-05-12 04:02:03 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc :argumanets count
+ *@argv: array of argumanets strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		int sum = 0, i;
+
+		for (i = 1; i < argc; i++)
+		{
+			int x = atoi(argv[i]);
+
+			if (x)
+			{
+				sum += x;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
