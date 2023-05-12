27b368db705e1dcbbe1bd1922190bbd921bcfdee
nadar99 <rnada260@gmail.com>
2023-05-12 03:41:28 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *@argc :argumanets count
+ *@argv: array of argumanets strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
