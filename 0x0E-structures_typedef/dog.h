7acd651ca25e14075a668bf2fe10b98cb413300a
nadar99 <rnada260@gmail.com>
2023-05-22 05:10:40 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef FILE_NAME
+#define FILE_NAME
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
