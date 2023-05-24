cf73009054628395f2b736bee5c75450edec1294
nadar99 <rnada260@gmail.com>
2023-05-24 04:04:51 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - prints a name as is
  * @name: name of the person
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
