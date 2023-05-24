8922afecb1bf1aade5a149249b8333489a9be80a
nadar99 <rnada260@gmail.com>
2023-05-24 04:02:48 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f : function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
