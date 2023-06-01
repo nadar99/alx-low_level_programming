5a676524703b932ed53e48936dd9b645b558f11f
nadar99 <rnada260@gmail.com>
2023-06-01 11:32:35 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * before_main - print before the main is executed.
+ *
+ * Return: void.
+ */
+void before_main(void) __attribute__((constructor));
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
