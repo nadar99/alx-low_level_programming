54ac730ef10e882652e54d8d8b1e51508d0d61e8
nadar99 <rnada260@gmail.com>
2023-06-01 09:50:47 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * len - return the length of a string.
+ *@str: string
+ * Return: length of a string.
+ */
+unsigned int len(const char *str)
+{
+	unsigned int n = 0;
+
+	while (*str != '\0')
+	{
+		n++;
+		str++;
+	}
+	return (n);
+}
+
+/**
+ *add_node - adds a new node at the beginning of a list_t list.
+ *@head : list.
+ *@str : string to add.
+ *Return: the address of the new node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	if (!new->str)
+		return (NULL);
+	new->len = len(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
