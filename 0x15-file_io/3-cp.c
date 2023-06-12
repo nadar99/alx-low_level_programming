91676ea67e8b564c66b5ee68901d42319828a096
nadar99 <rnada260@gmail.com>
2023-06-12 14:09:30 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,20 +35,25 @@ int main(int ac, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	buffer = malloc(1024 * sizeof(char));
 	rcount = read(file_from, buffer, 1024);
-	if (file_from == -1 || rcount == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	wcount = write(file_to, buffer, rcount);
-	if (file_to == -1 || wcount == -1)
+	do
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buffer);
-		exit(99);
-	}
+		if (file_from == -1 || rcount == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+        	wcount = write(file_to, buffer, rcount);
+		if (file_to == -1 || wcount == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		rcount = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rcount > 0);
 	close_file(file_from);
 	close_file(file_to);
 	free(buffer);
